6b141b90b46a54100e797f1ede83bdbe83006c19
russellspencer <russellspencer@users.noreply.github.com>
2018-04-15 11:32:42 +0100

    tidy up 15-04-18

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
 $(document).foundation();
 
 window.sr = ScrollReveal({mobile: false});
-sr.reveal('.card-1', { duration: 2000, origin:'left', distance:'200%', opacity:0, scale:1 });
-sr.reveal('.card-2', { delay: 0, duration: 2000, origin:'right', distance:'200%', opacity:0, scale:1 });
+sr.reveal('.card-1', { duration: 2000, origin:'left', distance:'0%', opacity:-1, scale:1 });
+sr.reveal('.card-2', { delay: 0, duration: 2000, origin:'right', distance:'0%', opacity:-1, scale:1 });
